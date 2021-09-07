3950236c221f6a5afc933ef18e902048727d7f2a
Andrew <andslobby@gmail.com>
2021-09-07 19:31:01 +0300

    chore: refresh links to BE

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: "https://k9tqg1fapd.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://cqtw2x2oqd.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://k9tqg1fapd.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
