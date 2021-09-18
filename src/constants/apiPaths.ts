f526e62e1fcaf575cc9d28bbdb16514b09750626
Andrew <andslobby@gmail.com>
2021-09-18 20:18:31 +0300

    refactor: fix path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://k9tqg1fapd.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://ozeaujdmh7.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://k9tqg1fapd.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
